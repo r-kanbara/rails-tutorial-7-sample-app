525c878fd839e668e60e6dbeede7f32d0e69b80c
reo <r-kanbara@ruby-dev.jp>
2024-02-13 11:51:57 +0900

    9.3.1 [Remember me]ボックスをテストする

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -74,3 +74,19 @@ class LogoutTest < Logout
     assert_not @user.authenticated?('')
   end
 end
+
+class RememberingTest < UsersLogin
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    # Cookie を保存してログイン
+    log_in_as(@user, remember_me: '1')
+    # Cookie が削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+end
