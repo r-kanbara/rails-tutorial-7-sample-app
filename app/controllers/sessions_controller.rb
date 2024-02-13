d962c718567271bd3d31d63d5e935a3874e7dbbf
reo <r-kanbara@ruby-dev.jp>
2024-02-13 11:16:28 +0900

    9.1.4 2つの目立たないバグ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other  # Turboを使用するときは，このステータスを指定する必要がある
   end
 end
