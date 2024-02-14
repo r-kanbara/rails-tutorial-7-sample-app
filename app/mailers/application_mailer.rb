b754eca9aea60250d9af4dc7474b55afdbd10c81
reo <r-kanbara@ruby-dev.jp>
2024-02-14 16:58:50 +0900

    11.2.2 送信メールのプレビュー

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "user@realdomain.com"
+  default from: "user@realdomain.com"  # 本番環境では user@realdomain.com を自分が使用しているメールアドレスに変更する必要がある
   layout "mailer"
 end
