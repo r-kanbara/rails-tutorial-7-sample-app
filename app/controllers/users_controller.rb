c002c94a9cf1090142fcb62c92206e4d89013fda
reo <r-kanbara@ruby-dev.jp>
2024-02-15 13:04:42 +0900

    11.3.3 有効化のテストとリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def new
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,19 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # アカウントを有効にする
+  def activate
+    # update_columnsメソッドは，update_attributeメソッドと異なり，バリデーションやsave_beforeのようなコールバックの実行がされない
+    update_columns(activated: true, activated_at: Time.zone.now)
+    # update_attribute(:activated, true)
+    # update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
     # メールアドレスをすべて小文字にする
     def downcase_email
