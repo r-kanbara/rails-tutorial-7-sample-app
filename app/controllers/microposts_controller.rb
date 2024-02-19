975935602cc16bd191e0bb619dc83085a4eddd38
reo <r-kanbara@ruby-dev.jp>
2024-02-19 11:32:25 +0900

    13.3.4 マイクロポストを削除する

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -13,11 +14,25 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_back_or_to(root_url, status: :see_other)
+    # リダイレクト先について，下記のような記述も可
+    # if request.referrer.nil?
+    #   redirect_to root_url, status: :see_other
+    # else
+    #   redirect_to request.referrer, status: :see_other
+    # end
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url, status: :see_other if @micropost.nil?
     end
 end
