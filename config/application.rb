1923a76e9c2c04c52838f1cd210492115ec87711
reo <r-kanbara@ruby-dev.jp>
2024-02-19 15:04:39 +0900

    13.4.3 画像のリサイズ

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module SampleApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 画像処理に MiniMagick を利用する設定
+    config.active_storage.variant_processor = :mini_magick
   end
 end
