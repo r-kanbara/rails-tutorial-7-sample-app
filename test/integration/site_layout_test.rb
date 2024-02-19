b1faa7ede525886a22b360b57ed944546fc6a565
reo <r-kanbara@ruby-dev.jp>
2024-02-19 09:16:00 +0900

    13.3.2 マイクロポストを作成する

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -15,14 +15,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", "https://railstutorial.jp/", count: 2
+    assert_select "a[href=?]", "https://railstutorial.jp/", count: 1
     assert_select "a[href=?]", "https://www.michaelhartl.com/"
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", "https://news.railstutorial.org/"
-    assert_select "a[href=?]", signup_path
+    # assert_select "a[href=?]", signup_path
     assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", "https://rubyonrails.org/"
+    # assert_select "a[href=?]", "https://rubyonrails.org/"
 
     get contact_path
     assert_select "title", full_title("Contact")
